287a9a51df42018b375614095f09ea04f3f2915d
profgit2 <prof.git2@gmail.com>
2025-02-01 17:34:00 +0530

    new commit
    New Commit Test

--- src/test/java/com/ecom/test/RegistrationTest.java
+++ src/test/java/com/ecom/test/RegistrationTest.java
@@ -11,6 +11,17 @@ public static void main(String[] args) {
 		
 		System.out.println("Inside main method");
 		System.out.println("Inside main method");
+		
+		System.out.println("Inside main method");
+		System.out.println("Inside main method");
+		
+		System.out.println("Inside main method");
+		System.out.println("Inside main method");
+		
+		System.out.println("Inside main method");
+		System.out.println("Inside main method");
+		
+		
 	}
 
 }

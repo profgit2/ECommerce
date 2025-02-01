9cc289ba85148b8e698aa42331c7f89e62025704
profgit2 <prof.git2@gmail.com>
2025-02-01 17:35:49 +0530

    rr
    rr

--- src/test/java/com/ecom/test/RegistrationTest.java
+++ src/test/java/com/ecom/test/RegistrationTest.java
@@ -5,7 +5,7 @@ public class RegistrationTest {
 public static void main(String[] args) {
 		
 		System.out.println("Inside main method");
-		System.out.println("Inside main method");System.out.println("Inside main method");
+		System.out.println("Inside main method");System.out.println("Inside main meth--od"); 
 		
 		System.out.println("Inside main method");
 		

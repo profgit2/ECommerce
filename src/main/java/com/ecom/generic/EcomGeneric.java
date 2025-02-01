8dc1a012c51085e9c9d6c2b3eddef93aec3c955a
profgit2 <prof.git2@gmail.com>
2025-02-01 19:45:12 +0530

    test
    test

---
+++ src/main/java/com/ecom/generic/EcomGeneric.java
@@ -0,0 +1,11 @@
+package com.ecom.generic;
+
+public class EcomGeneric {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Inside the main method of EcomGeneric Class...");
+	}
+
+}

a2db56faab0b75fdcfb24f80a2181ab1c8c8af6b
profgit2 <prof.git2@gmail.com>
2026-03-04 00:41:24 +0530

    dddddd
    ddd

--- src/test/java/com/ecom/test/HomeTest.java
+++ src/test/java/com/ecom/test/HomeTest.java
@@ -4,39 +4,10 @@ public class HomeTest {
 
 public static void main(String[] args) {
 		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");System.out.println("Inside main meth--od"); 
-		
-		System.out.println("Inside main method");
-		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");
-		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");
-		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");
-		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");
-		
-		System.out.println("laxman More");
-		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");
-		
-		System.out.println("laxman More");
-		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");
-		
-		System.out.println("laxman More");
-		
-		System.out.println("Inside main method");
-		System.out.println("Inside main method");
-		
-		System.out.println("laxman More");
+	
+	System.out.println("Inside main method");
+	
+	
 		
 		
 	}
